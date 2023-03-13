40adef346f18c648550716b6860bd5106a0cfcd2
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2023-03-13 18:32:46 -0300

    fix: top level await vite config

--- src/lib/images/webps/index.ts
+++ src/lib/images/webps/index.ts
@@ -1,16 +1,14 @@
 export const IMAGES_WEBP = {
-	avatar: await import('./avatar.webp').then(i => i.default),
-	displacement: await import('./texture-displacement.webp').then(i => i.default),
-	grid: await import('./texture-grid.webp').then(i => i.default),
-	metalness: await import('./texture-metalness.webp').then(i => i.default),
-	noise: await import('./texture-noise.webp').then(i => i.default),
-	previewPortfolio: await import('./preview-portfolio.webp').then(i => i.default),
-	previewNextPlate: await import('./preview-next-plate.webp').then(i => i.default),
-	previewGithubUnfollowerChecker: await import('./preview-github-unfollow-checker.webp').then(
-		i => i.default
-	),
-	previewHotCode: await import('./preview-hotcode.webp').then(i => i.default),
-	profileCropped: await import('./profile-cropped.webp').then(i => i.default),
-	profile: await import('./profile.webp').then(i => i.default),
-	universe: await import('./bg-universe.webp').then(i => i.default)
+	avatar: (await import('./avatar.webp')).default,
+	displacement: (await import('./texture-displacement.webp')).default,
+	grid: (await import('./texture-grid.webp')).default,
+	metalness: (await import('./texture-metalness.webp')).default,
+	noise: (await import('./texture-noise.webp')).default,
+	previewPortfolio: (await import('./preview-portfolio.webp')).default,
+	previewNextPlate: (await import('./preview-next-plate.webp')).default,
+	previewGithubUnfollowerChecker: (await import('./preview-github-unfollow-checker.webp')).default,
+	previewHotCode: (await import('./preview-hotcode.webp')).default,
+	profileCropped: (await import('./profile-cropped.webp')).default,
+	profile: (await import('./profile.webp')).default,
+	universe: (await import('./bg-universe.webp')).default
 };
--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import viteCompression from 'vite-plugin-compression';
 import browserslistToEsbuild from 'browserslist-to-esbuild';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit(), viteCompression({ algorithm: 'brotliCompress' })],
+	plugins: [sveltekit(), topLevelAwait(), viteCompression({ algorithm: 'brotliCompress' })],
 	ssr: {
 		noExternal: ['three', 'troika-three-text']
+	},
+	build: {
+		target: browserslistToEsbuild()
 	},
 	define: {
 		'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
 	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	build: {
-		target: browserslistToEsbuild()
-	},
-	esbuild: {
-		supported: {
-			'top-level-await': true
-		}
 	}
 };
 
