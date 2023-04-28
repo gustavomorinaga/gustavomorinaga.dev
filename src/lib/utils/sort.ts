3d59a7afb011644d6b8083b053cceac0630cf9be
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2023-04-28 17:58:23 -0300

    fix: is sort array exists

--- src/lib/utils/sort.ts
+++ src/lib/utils/sort.ts
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export const sortBy = <T extends Record<string, any>, K extends keyof T>(arr: T[], k: K): T[] =>
-	arr.length ? arr.concat().sort((a, b) => (a[k] > b[k] ? 1 : a[k] < b[k] ? -1 : 0)) : [];
+	arr?.length ? arr.concat().sort((a, b) => (a[k] > b[k] ? 1 : a[k] < b[k] ? -1 : 0)) : [];
