8b29715ea539dda306fa601d36600fcec4d7b79d
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2023-05-03 16:19:38 -0300

    test: prerender in blog slug page

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,6 +1,13 @@
 import { COMPILER_CONFIG, clearExtraContent } from '$lib/utils';
 import { compile } from 'mdsvex';
 
+export const prerender = false;
+export const config = {
+	isr: {
+		expiration: 60
+	}
+};
+
 export const load = async ({ parent }) => {
 	const data = await parent();
 
