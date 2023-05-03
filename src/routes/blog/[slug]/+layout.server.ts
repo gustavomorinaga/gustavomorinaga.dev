01fd797eb196412ce4b1fd11ead7c22dd010ff58
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2023-05-03 01:02:04 -0300

    fix: blog slug prerender

--- src/routes/blog/[slug]/+layout.server.ts
+++ src/routes/blog/[slug]/+layout.server.ts
@@ -4,6 +4,9 @@ import type { ICMSData, IPost } from '$lib/ts';
 
 const PAGE_SIZE = 3;
 
+export const prerender = 'auto';
+export const config = {};
+
 export const load = async ({ fetch, params: { slug } }) => {
 	const post = await fetch(`/api/posts/${slug}`).then<IPost>(res => res.json());
 
