d574a24542291d31fc3a7e12fae4387ddc112227
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2023-05-03 13:55:52 -0300

    revert: prerender blog slug

--- src/routes/blog/[slug]/+layout.server.ts
+++ src/routes/blog/[slug]/+layout.server.ts
@@ -1,5 +1,7 @@
 import type { IPost } from '$lib/ts';
 
+export const prerender = 'auto';
+
 export const load = async ({ fetch, params: { slug } }) => {
 	const post = await fetch(`/api/posts/${slug}`).then<IPost>(res => res.json());
 
