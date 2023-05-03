f4b03752b5241cecff8dfa5163218d28166b6460
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2023-05-03 13:49:06 -0300

    fix: removing prerender option in blog slug

--- src/routes/blog/[slug]/+layout.server.ts
+++ src/routes/blog/[slug]/+layout.server.ts
@@ -1,7 +1,5 @@
 import type { IPost } from '$lib/ts';
 
-export const prerender = 'auto';
-
 export const load = async ({ fetch, params: { slug } }) => {
 	const post = await fetch(`/api/posts/${slug}`).then<IPost>(res => res.json());
 
