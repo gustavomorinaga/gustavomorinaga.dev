edf6f370a841d95b982070e8342ca7cd83901ca4
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2023-05-05 02:33:59 -0300

    fix: page transition bug on blog slug layout

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,9 +1,16 @@
 import { compileContent } from '$lib/utils';
+import type { IPost } from '$lib/ts';
 
-export const load = async ({ parent }) => {
-	const data = await parent();
+export const prerender = false;
+export const config = {
+	isr: {
+		expiration: 60
+	}
+};
 
-	const content = await compileContent(data.post.content);
+export const load = async ({ fetch, params: { slug } }) => {
+	const post = await fetch(`/api/posts/${slug}`).then<IPost>(res => res.json());
+	const content = await compileContent(post.content);
 
-	return { content };
+	return { post, content };
 };
