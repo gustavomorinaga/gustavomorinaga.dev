cd24ada49512741a21da80bb2c708a3dbaab3469
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2023-05-08 22:14:11 -0300

    fix: card-knowledge translation

--- src/lib/components/cards/card-knowledge/card-knowledge.svelte
+++ src/lib/components/cards/card-knowledge/card-knowledge.svelte
@@ -4,36 +4,40 @@
 
 	export let knowledge: IKnowledgePayload;
 
-	const { title, skills } = knowledge;
+	$: ({ title, skills } = knowledge);
 </script>
 
 <article class="knowledge">
-	<h3>{title}</h3>
+	<div class="card-body">
+		<h3>{title}</h3>
 
-	<ul>
-		{#each skills as { icon, collection, title, url, color }}
-			<li data-tip={title} style="--icon-color: {color}">
-				<a href={url} target="_blank" rel="noopener noreferrer">
-					<Icon {icon} {collection} size="lg" />
-				</a>
-			</li>
-		{/each}
-	</ul>
+		<ul>
+			{#each skills as { icon, collection, title, url, color }}
+				<li data-tip={title} style="--icon-color: {color}">
+					<a href={url} target="_blank" rel="noopener noreferrer">
+						<Icon {icon} {collection} size="lg" />
+					</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
 </article>
 
 <style lang="scss" global>
 	article.knowledge {
-		@apply flex-auto card card-bordered p-8 bg-base-100/75 shadow-lg backdrop-blur-md;
+		@apply flex-auto card card-compact md:card-normal card-bordered bg-base-100/75 shadow-lg backdrop-blur-md;
 
-		& h3 {
-			@apply text-2xl font-futuristic text-shadow-glow shadow-primary mb-4;
-		}
+		& > .card-body {
+			& h3 {
+				@apply text-xl md:text-2xl font-futuristic text-shadow-glow shadow-primary mb-4;
+			}
 
-		& > ul {
-			@apply flex flex-wrap gap-8;
+			& > ul {
+				@apply flex flex-wrap gap-4 md:gap-8;
 
-			& li {
-				@apply tooltip tooltip-bottom lg:hover:text-[var(--icon-color)] transition-colors duration-700 ease-smooth;
+				& li {
+					@apply tooltip tooltip-bottom lg:hover:text-[var(--icon-color)] transition-colors duration-700 ease-smooth;
+				}
 			}
 		}
 	}
