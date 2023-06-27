59cb6d6adb4d6975f6926389b7a1813f4c053c9b
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2023-06-26 23:51:37 -0300

    chore: eslint-plugin-svelte upgrade

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,14 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'prettier',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+	],
+	plugins: ['@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020
