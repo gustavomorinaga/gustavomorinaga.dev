e69a27eeeda92435d938b8bd50c8b8ba421dd15e
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2023-07-01 17:25:18 -0300

    fix: postcss and cssnano config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,30 @@
 module.exports = {
 	root: true,
-	parser: '@typescript-eslint/parser',
 	extends: [
 		'eslint:recommended',
-		'prettier',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:svelte/recommended',
+		'prettier'
 	],
+	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
 	},
 	env: {
 		browser: true,
 		es2017: true,
 		node: true
-	}
+	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	]
 };
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,3 +1,10 @@
-module.exports = {
-	plugins: [require('tailwindcss'), require('autoprefixer'), require('cssnano')]
-};
+const config = {
+	plugins: [
+		require('tailwindcss'),
+		require('postcss-nested'),
+		require('autoprefixer'),
+		require('cssnano')({ preset: 'default' })
+	]
+}
+
+module.exports = config;
