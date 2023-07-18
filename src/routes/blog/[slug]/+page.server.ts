d77db3160cc53e782236f7dceaf82bcb324bd5a3
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2023-07-18 01:13:06 -0300

    fix: remove isr configs

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -2,13 +2,6 @@ import { compileContent } from '$lib/utils';
 import { error } from '@sveltejs/kit';
 import type { IPost } from '$lib/ts';
 
-export const prerender = false;
-export const config = {
-	isr: {
-		expiration: 60 * 60 // 1 hour
-	}
-};
-
 export const load = async ({ fetch, params: { slug } }) => {
 	const response = await fetch(`/api/posts/${slug}`);
 	if (!response.ok) throw error(404, 'Not Found');
